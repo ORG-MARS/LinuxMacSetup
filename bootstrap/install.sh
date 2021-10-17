772fe1b2749a8a6e7ff1f2f27d8403477644aea8
suzukaze hazuki <suzukaze.hazuki2020@gmail.com>
2021-10-17 22:52:06 +0900

    add config

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -199,6 +199,7 @@ homebrew() {
   # 安装oh-my-zsh
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+  # p10k configure 可以重新配置
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 
