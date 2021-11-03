6b7a705cb1aefc802e7216d937fa781780be3c0e
Hu Peng <ko.h@ctw.inc>
2021-11-03 19:27:42 +0900

    fix docker url

--- docker/init_docker.sh
+++ docker/init_docker.sh
@@ -4,7 +4,7 @@ sh get-docker.sh
 #chmod 666 /var/run/docker.sock
 usermod -aG docker $USER
 
-cp $BASE_DIR/docker/daemon.json /etc/docker/daemon.json
+cp $BASE_DIR/source/docker/daemon.json /etc/docker/daemon.json
 
 mkdir -p /etc/systemd/system/docker.service.d
 systemctl daemon-reload
