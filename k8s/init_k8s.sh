2456d259ecfddc85645946e3b90bfc0794805a1b
Hu Peng <ko.h@ctw.inc>
2021-11-03 12:13:26 +0900

    add echo

--- k8s/init_k8s.sh
+++ k8s/init_k8s.sh
@@ -4,5 +4,7 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl kubeadm
+echo k8s版本
 kubectl version
+echo kbadm版本
 kubeadm version
