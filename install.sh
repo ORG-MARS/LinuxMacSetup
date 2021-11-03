d6e3a53b6855231bdc0e4b11f456eb19285b70c1
Hu Peng <ko.h@ctw.inc>
2021-11-03 09:17:53 +0900

    修改文件路径

--- install.sh
+++ install.sh
@@ -10,33 +10,33 @@ git checkout ubuntu20
 ln -s "$BASE_DIR/source" "$BASE_DIR/config"
 
 # 初始化ubuntu
-sh ./ubuntu/init_ubuntu.sh
+sh $BASE_DIR/source/ubuntu/init_ubuntu.sh
 
 # 配置inputrc
-sh ./input/init_inputrc.sh
+sh $BASE_DIR/source/input/init_inputrc.sh
 
 # 配置env
-sh ./env/init_env.sh
+sh $BASE_DIR/source/env/init_env.sh
 
 # 安装vim
-sh ./vim/init_vim.sh
+sh $BASE_DIR/source/vim/init_vim.sh
 
 # 安装zsh
-sh ./zsh/init_zsh.sh
+sh $BASE_DIR/source/zsh/init_zsh.sh
 
 # 安装fzf
-sh ./fzf/init_fzf.sh
+sh $BASE_DIR/source/fzf/init_fzf.sh
 
 # 安装并开启ssh
-sh ./ssh/init_ssh.sh
+sh $BASE_DIR/source/ssh/init_ssh.sh
 
 # 安装docker
-sh ./docker/init_docker.sh
+sh $BASE_DIR/source/docker/init_docker.sh
 
 # 安装k8s及周边工具
-sh ./k8s/init_k8s.sh
-sh ./k8s/init_kubeadm.sh
-sh ./k8s/init_kubectx.sh
+sh $BASE_DIR/source/k8s/init_k8s.sh
+sh $BASE_DIR/source/k8s/init_kubeadm.sh
+sh $BASE_DIR/source/k8s/init_kubectx.sh
 
 # 安装terraform
-sh ./terraform/init_terraform.sh
+sh $BASE_DIR/source/terraform/init_terraform.sh
