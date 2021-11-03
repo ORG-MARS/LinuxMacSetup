29d4eefff6deb5248a404e94ca8f4ff80b387bad
Hu Peng <ko.h@ctw.inc>
2021-11-03 20:18:29 +0900

    add md

--- ssh/init_ssh.sh
+++ ssh/init_ssh.sh
@@ -3,7 +3,7 @@ apt install openssh-server -y
 systemctl status ssh
 ufw allow ssh
 
-# 创建ssh密钥
+# 创建ssh密钥 密码是一个空格
 ssh-keygen -q -N " " -f ~/.ssh/id_rsa
 chmod 700 ~/.ssh
 touch ~/.ssh/authorized_keys
