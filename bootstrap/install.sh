8e8af110a0534a033c339d822976878ad71138e6
Hu Peng <ko.h@ctw.inc>
2021-11-05 14:53:16 +0900

    quick search vpn

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -127,6 +127,8 @@ homebrew() {
 
   # vpn工具
   brew install viscosity
+  git clone https://github.com/andrewschleifer/viscosity-alfredworkflow.git $HOME/Library/Application\ Support/Alfred/Alfred.alfredpreferences/workflows/viscosity
+
 
   # vscode
   brew install visual-studio-code
